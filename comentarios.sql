2896f0bcf45defd148c90a6639103c704e29935e
JuanQuezada <ju.quezada.c@gmail.com>
2021-10-27 01:06:59 -0300

    terminado

--- comentarios.sql
+++ comentarios.sql
@@ -10,22 +10,22 @@ CREATE TABLE comentarios(
 );
 --2. Crear 2 comentarios para el post de "Pamela" y 4 para "Carlos". (0.4 Puntos)
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES (1,'2021-10-25','19:00:00','primer comentario');
+VALUES (1,'2021-10-25','18:00:00','primer comentario');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES (2,'2021-10-25','19:00:00','segundo comentario');
+VALUES (2,'2021-10-25','18:30:00','segundo comentario');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES (6,'2021-10-25','19:00:00','primer comentario');
+VALUES (6,'2021-10-25','19:00:05','primer comentario');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES (6,'2021-10-25','19:00:00','segundo comentario');
+VALUES (6,'2021-10-25','19:00:10','segundo comentario');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES (6,'2021-10-25','19:00:00','tercer comentario');
+VALUES (6,'2021-10-25','19:00:20','tercer comentario');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES (6,'2021-10-25','19:00:00','cuarto comentario');
+VALUES (6,'2021-10-25','19:00:30','cuarto comentario');
 
 --3. Crear un nuevo post para "Margarita". (1 Punto)
 INSERT INTO post (nombre_usuario, fecha_creacion, contenido, descripcion, titulo)
@@ -37,16 +37,16 @@ INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
 VALUES ( 7,'2021-10-25','19:00:00','primer contenido');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES ( 7,'2021-10-25','19:00:00','segundo contenido');
+VALUES ( 7,'2021-10-25','19:05:05','segundo contenido');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES ( 7,'2021-10-25','19:00:00','tercer contenido');
+VALUES ( 7,'2021-10-25','19:10:10','tercer contenido');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES ( 7,'2021-10-25','19:00:00','cuarto contenido');
+VALUES ( 7,'2021-10-25','19:15:15','cuarto contenido');
 
 INSERT INTO comentarios (id_post, fecha, hora_de_creacion, contenido)
-VALUES ( 7,'2021-10-25','19:00:00','quinto contenido');
+VALUES ( 7,'2021-10-25','19:20:20','quinto contenido');
 
 
 select * from post left join comentarios on post.id=comentarios.id_post;
